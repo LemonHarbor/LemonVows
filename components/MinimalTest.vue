91c98e6424aaeb0fa479dbe4c255c1445e99545a
LemonHarbor <lemonharbor@gmx.de>
2025-04-01 18:48:59 +0200

    Add MinimalTest component

---
+++ components/MinimalTest.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="minimal-test">
+    Minimaler Test erfolgreich!
+  </div>
+</template>
+
+<script>
+// Absichtlich leer oder traditionelles export default {}
+export default {
+  name: 'MinimalTest'
+}
+</script>
+
+<style scoped>
+.minimal-test {
+  padding: 10px;
+  border: 1px dashed green;
+  color: green;
+}
+</style>
